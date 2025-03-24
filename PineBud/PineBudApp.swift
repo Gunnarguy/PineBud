a5b8e90d4d3ae95ca48c810f90e3d90fd29a309c
gunnarguy <gunnarguy@me.com>
2025-03-23 19:49:19 -0700

    Initial Commit

---
+++ PineBud/PineBudApp.swift
@@ -0,0 +1,17 @@
+//
+//  PineBudApp.swift
+//  PineBud
+//
+//  Created by Gunnar Hostetler on 3/23/25.
+//
+
+import SwiftUI
+
+@main
+struct PineBudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
